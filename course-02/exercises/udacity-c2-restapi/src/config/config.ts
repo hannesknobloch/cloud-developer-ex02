f4c6c4d777ab8d69df9a385caa759ee3c277a38f
Robot Ini-Bitbucket <inventory-intelligence@idealo.de>
2022-08-15 16:03:51 +0200

    building and deploying done

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "udagramhannesdev",
-    "password": "W31m4r56.",
-    "database": "udagramhannesdev",
-    "host": "udagramhannesdev.cvejfyyn61x3.us-east-1.rds.amazonaws.com",
-    "dialect": "postgres",
-    "aws_region": "us-east-1",
-    "aws_profile": "udagram-dev",
-    "aws_media_bucket": "udagram-eberhard-dev"
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
+    "dialect": process.env.DIALECT,
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "jwt": {
     "secret": " "
