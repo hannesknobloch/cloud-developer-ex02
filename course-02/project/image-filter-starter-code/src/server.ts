dfc56d3f9478213b49ef1bc94b510e9184e9ebba
Robot Ini-Bitbucket <inventory-intelligence@idealo.de>
2022-08-16 10:47:21 +0200

    project working

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,3 +1,4 @@
+import { Router, Request, Response } from 'express';
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
@@ -15,20 +16,34 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
+  app.get("/filteredimage/", 
+    async (req, res) => {
+    // endpoint to filter an image from a public url.
+   // IT SHOULD
+
+    //    1. validate the image_url query
+    let { image_url } = req.query;
+    if ( !image_url ) {
+      return res.status(400).send(`url is required`)
+    };
+
+    //    2. call filterImageFromURL(image_url) to filter the image
+    let filteredpath = await filterImageFromURL(image_url);
+    
+    //    3. send the resulting file in the response
+    res.status(200).sendFile(filteredpath, function() {
+      //    4. deletes any files on the server on finish of the response
+      deleteLocalFiles([filteredpath]);
+    });
+  
+  });
+
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-
   //! END @TODO1
   
   // Root Endpoint
